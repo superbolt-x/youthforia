de87efab1ec97b812535a59f685cb37dca4827b1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-20 12:34:06 -0500

    Update facebook_ad_performance.sql
    preview_links

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -22,6 +22,7 @@ visual,
 copy,
 format_visual,
 visual_copy,
+preview_links,
 date,
 date_granularity,
 spend,
@@ -31,3 +32,4 @@ add_to_cart,
 purchases,
 revenue
 FROM {{ ref('facebook_performance_by_ad') }}
+LEFT JOIN (SELECT link as preview_links, ad_name FROM {{ source('gsheet_raw','meta_preview_links') }} USING(ad_name)
