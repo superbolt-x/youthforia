119b702a309b423a680390216da2fe0f7cdd24b8
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-07 15:32:56 -0500

    Update facebook_ad_performance.sql

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -16,7 +16,7 @@ adset_id,
 adset_effective_status,
 audience,
 ad_name,
-ad_id,
+ad_id::varchar,
 ad_effective_status,
 visual,
 copy,
@@ -32,4 +32,4 @@ add_to_cart,
 purchases,
 revenue
 FROM {{ ref('facebook_performance_by_ad') }}
-LEFT JOIN (SELECT preview_link as preview_links, ad_id FROM {{ source('gsheet_raw','meta_preview_links_automated') }}) USING(ad_id)
+LEFT JOIN (SELECT preview_link as preview_links, ad_id::varchar FROM {{ source('gsheet_raw','meta_preview_links_automated') }}) USING(ad_id)
